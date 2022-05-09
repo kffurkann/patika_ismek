585636990b3a932a44e56cad80955e9ffc20a522
kffurkann <101575343+kffurkann@users.noreply.github.com>
2022-05-10 00:33:08 +0300

    Create odev5.sql

---
+++ odev5.sql
@@ -0,0 +1,18 @@
+-- 1.SORU
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+-- 2.SORU
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+
+-- 3.SORU
+SELECT last_name, store_id FROM customer
+WHERE store_id=1
+ORDER BY last_name DESC
+LIMIT 4;
