0816b680cf6b2ec17094ed482c65c03d2862d4f5
kffurkann <101575343+kffurkann@users.noreply.github.com>
2022-05-13 00:08:13 +0300

    Create odev9.sql

---
+++ odev9.sql
@@ -0,0 +1,14 @@
+-- 1.SORU
+SELECT city,country
+FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+-- 2.SORU
+SELECT customer.first_name,customer.last_name,payment.payment_id
+FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id;
+
+-- 3.SORU
+SELECT customer.first_name,customer.last_name,rental.rental_id
+FROM customer
+INNER JOIN rental ON rental.customer_id = customer.customer_id;
