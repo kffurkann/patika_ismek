3f742dea88020cd46a3f946d2622158c79272f78
kffurkann <101575343+kffurkann@users.noreply.github.com>
2022-05-14 20:06:52 +0300

    Create odev11.sql

---
+++ odev11.sql
@@ -0,0 +1,29 @@
+-- 1.SORU
+(SELECT first_name FROM actor)
+UNION
+(SELECT first_name FROM customer);
+
+-- 2.SORU
+(SELECT first_name FROM actor)
+INTERSECT
+(SELECT first_name FROM customer);
+
+--3.SORU
+(SELECT first_name FROM actor)
+EXCEPT
+(SELECT first_name FROM customer);
+
+-- 4.SORU
+(SELECT first_name FROM actor)
+UNION ALL
+(SELECT first_name FROM customer);
+
+
+(SELECT first_name FROM actor)
+INTERSECT ALL
+(SELECT first_name FROM customer);
+
+
+(SELECT first_name FROM actor)
+EXCEPT ALL
+(SELECT first_name FROM customer);
